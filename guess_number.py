4d12f238fa6ea326837c365055c801375f66c1a3
yunyan <281390230@qq.com>
2019-04-03 20:56:06 +0800

    4.3

--- guess_number.py
+++ guess_number.py
@@ -1,21 +1,33 @@
-# -*- coding: encoding -*-
+# -*- coding: utf-8 -*- 
  
 import random
 i=random.randrange(1,101)
-print(i)
-x = input("请输入一个1-100之间的整数： ")
-def check_num(x):
-    if  x<1:
-        print("小于1了，请输入一个1-100之间的整数：")
-    elif x>100:
-        print("大于100了，请输入一个1-100之间的整数：")
-    elif isinstance(x,int)==False:
-        print("格式错误，请输入一个1-100之间的整数：")
+while True:
+    x = int(input("请输入一个1-100之间的整数： "))
+    if x<i:
+        print("猜小了，再试试")
+    elif x>i:
+        print("猜大了，再试试")
+    else :
+        print("恭喜你，猜对了") 
+        break
+    
+
+def check_type(x):
+    if x.isnumeric():
+        return True
     else:
-        pass    
-if x<i:
-    print("猜小了，再试试")
-elif x>i:
-    print("猜大了，再试试")
-else :
-    print("恭喜你，猜对了")  
+        return False
+
+def check_num():
+        if x < 1:
+            print("小于1了，请输入一个1-100之间的整数：")
+        elif x > 100:
+                print("大于100了，请输入一个1-100之间的整数：")
+        else:
+            pass
+
+
+
+
+    
